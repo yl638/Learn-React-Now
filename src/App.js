f7c9497983a130e1bfa908000af2cf9686515cf2
Yujie Lin <yl638@student.le.ac.uk>
2021-06-15 16:29:24 +0100

    first test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const url = 'http://localhost:8080/api/Messages'
+    const [Message, setMessages] = useState(null);
+    useEffect(() => {
+        axios.get(url)
+            .then((response) => {
+                setMessages(response.data)
+            })
+    }, [url]);
+
+        if (Message) {
+        return (
+         <div>
+              <h1> {Message.theMessage} </h1>
+         </div>
+        );
+        }
+    return (
+        <div>
+        </div>
+        )
 }
+
+export default App;
 
 export default App;
